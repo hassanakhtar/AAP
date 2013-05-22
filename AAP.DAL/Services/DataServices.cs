afdc5260382ef44d57a457d65fe4df16eb6c79eb
Hassan Akhtar <hakhtar@folio3.com>
2013-05-22 12:18:49 +0500

    Database entities added

---
+++ AAP.DAL/Services/DataServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AAP.DAL
+{
+	public class DataServices : IDataServices
+	{
+		AapDataEntities db;
+
+		public DataServices()
+		{
+			db = new AapDataEntities();
+		}
+
+		/// <summary>
+		/// Gets the branch by code.
+		/// </summary>
+		/// <param name="branchCode">The branch code.</param>
+		/// <returns></returns>
+		public Branch GetBranchByCode(string branchCode)
+		{
+			return db.Branch.FirstOrDefault(b => b.Code == branchCode);
+		}
+
+	}
+}
---
+++ AAP.DAL/Services/IDataServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AAP.DAL
+{
+	interface IDataServices
+	{
+		/// <summary>
+		/// Gets the branch by code.
+		/// </summary>
+		/// <param name="branchCode">The branch code.</param>
+		/// <returns></returns>
+		Branch GetBranchByCode(string branchCode);
+	}
+}
