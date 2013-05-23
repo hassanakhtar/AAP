49dc25b7ec95abd5add187ffc33b2bc62bd38698
Hassan Akhtar <hakhtar@folio3.com>
2013-05-23 19:51:44 +0500

    Added some explicit cast operators, implemented TODOs on BLL Services

--- APP.BLL/Objects/Student.cs
+++ APP.BLL/Objects/Student.cs
@@ -22,5 +22,27 @@ namespace APP.BLL.Objects
 		public string GuardianCNIC { get; set; }
 		public DateTime? LastModified { get; set; }
 		public bool? IsArchive { get; set; }
+
+		public static explicit operator AAP.DAL.Student(Student student)
+		{
+			return new AAP.DAL.Student()
+			{
+				Id = student.Id,
+				FirstName = student.FirstName,
+				LastName = student.LastName,
+				Gender = student.Gender,
+				Religion = student.Religion,
+				DateOfBirth = student.DateOfBirth,
+				Picture = student.Picture,
+				Guardian = student.Guardian,
+				Relation = student.Relation,
+				Comments = student.Comments,
+				AddressId = student.AddressId,
+				ClassId = student.ClassId,
+				GuardianCNIC = student.GuardianCNIC,
+				LastModified = student.LastModified,
+				IsArchive = student.IsArchive
+			};
+		}
 	}
 }
--- APP.BLL/Services/Implementations/CourseService.cs
+++ APP.BLL/Services/Implementations/CourseService.cs
@@ -8,10 +8,21 @@ namespace APP.BLL.Services
 {
 	public class CourseService : ICourseService
 	{
+		public AAP.DAL.IDataServices DataService { get; set; }
+
+		public CourseService()
+		{
+			DataService = new AAP.DAL.DataServices();
+		}
+
+		/// <summary>
+		/// Adds the course.
+		/// </summary>
+		/// <param name="course">The course.</param>
+		/// <returns></returns>
 		public bool AddCourse(Course course)
 		{
-			//TODO: Implement This
-			return false;
+			return DataService.AddCourse((AAP.DAL.Course)course);
 		}
 	}
 }
