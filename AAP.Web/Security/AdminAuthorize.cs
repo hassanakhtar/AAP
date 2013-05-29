a5a8381c5a9c750faec9bbc46367a8e8b38938d1
Ovais Ashraf <oashraf@folio3.com>
2013-05-29 16:25:01 +0500

    -Membership + Role Management -Base Controller added -Simple Membership Data script having Roles insertion and Admin User insertion ( UserName=Admin : Password=click123 )

---
+++ AAP.Web/Security/AdminAuthorize.cs
@@ -0,0 +1,39 @@
+﻿using AAP.Web.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace AAP.Web.Security
+{
+	public class AdminAuthorize : AAP.Web.Security.AuthorizeAttribute
+	{
+		public AdminAuthorize()
+		{
+			Roles = "Admin";
+		}
+	}
+	public class ManagementAuthorize : AAP.Web.Security.AuthorizeAttribute
+	{
+		public ManagementAuthorize()
+		{
+			Roles = "Management";
+		}
+	}
+	public class TeacherAuthorize : AAP.Web.Security.AuthorizeAttribute
+	{
+		public TeacherAuthorize()
+		{
+			Roles = "Teacher";
+		}
+	}
+	public class StudentAuthorize : AAP.Web.Security.AuthorizeAttribute
+	{
+		public StudentAuthorize()
+		{
+			Roles = "Student";
+		}
+	}
+}
